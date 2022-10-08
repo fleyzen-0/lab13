bb2005c24f6009613d6d8e0e39b10c28593dc0b2
“Your <yaroslavdisiak@gmail.com>
2022-10-08 10:14:40 +0300

    done lab_ex_1

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-number1 = int(input("Введіть два числа "))
-number2 = int(input())
-while number1 >= number2:
-    print(number1)
-    number1 += 1
+try:
+    number1 = int(input("Введіть два числа: "))
+    number2 = int(input())
+    while number1 <= number2:
+        c = (number1 + number1)
+        number1 += 1
+    print("Cума чисел у вказаному діапазоні: ", c)
+    print("Середнє арифметичне: ", c / 2)
+
+except Exception as ex:
+    print("Erorr: ", ex);
+finally:
+    print("Завдання виконано.")
+
+
