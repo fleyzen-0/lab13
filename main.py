ed793431206f8f4df3761d7103d3d7b765331493
“Your <yaroslavdisiak@gmail.com>
2022-10-08 11:09:01 +0300

    done lab_ex_4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 try:
     line = int(input("Введіть довжину лінії: "))
-    simvol = input("Введіть символ для заповнення")
+    simvol = input("Введіть символ для заповнення: ")
     for item in range(0, line):
         print(simvol, end="")
 
