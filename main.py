8b0911ab931c91c176eb58e785eae7f331d378fa
“Your <yaroslavdisiak@gmail.com>
2022-10-08 11:06:35 +0300

    done lab_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-line = int(input("Введіть довжину лінії"))
+try:
+    line = int(input("Введіть довжину лінії: "))
+    for item in range(0, line):
+        print("*", end="")
+
+
+
+except Exception as ex:
+    print("Erorr: ", ex)
+finally:
+    print(" Завдання виконано.")
