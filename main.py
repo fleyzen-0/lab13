b3afbf69ab056043437e5f18c274313f6545bc8b
“Your <yaroslavdisiak@gmail.com>
2022-10-08 10:53:02 +0300

    done lab_ex_2

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-chislo = int(input("Введіть одне число: "))
-while chislo != 1:
-    print(chislo * chislo)
-    chislo -= 1
+try:
+    number = int(input("numner = "))
+    print(f'!{number} = ', end=" ")
+    temp = 1
+    for item in range(1, number+1):
+        if item == number:
+            print(item, end=" ")
+        else:
+            print(f'{temp} *', end=' ')
+
+        temp*=item
+    print(f'= {temp}')
+
+except Exception as ex:
+    print("Erorr: ", ex);
+finally:
+    print("Завдання виконано.")
+
+
